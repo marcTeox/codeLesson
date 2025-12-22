5351460971fba762783bd790306cdda6b702dd81
Marc <inc.kz.marc@gmail.com>
2025-12-22 19:33:19 +0800

    feat: add absolute path

--- utils/addNewSighting.js
+++ utils/addNewSighting.js
@@ -4,14 +4,15 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 
 export async function addNewSighting(newSighting) {
   try {
     const response = await fetch("http://localhost:3000/api");
     const data = await response.json();
+    const filePath = path.join(__dirname, "..", "data", "data.json");
     data.push(newSighting);
-    fs.writeFile("data/data.json", JSON.stringify(data, null, 2), (err) => {
+    console.log(filePath);
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
       if (err) {
         console.log(err);
         return;
